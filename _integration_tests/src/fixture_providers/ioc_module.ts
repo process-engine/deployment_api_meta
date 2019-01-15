51f904f7043da34cdcbd32423505c864397f4621
ElRaptorus <christian.werner@5minds.de>
2019-01-15 09:30:17 +0100

    :fire: Migrate Integrationtests to Runtime There is no need to run these tests twice, since this API is not modified that often (if ever).

--- _integration_tests/src/fixture_providers/ioc_module.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import {IamServiceMock} from '../mocks/index';
-
-export function registerInContainer(container: any): void {
-  // This removes the necessity for having a running IdentityServer during testing.
-  container.register('IamService', IamServiceMock);
-}
